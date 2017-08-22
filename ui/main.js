be8c71b5a86ad9d5e4c53f5aa7cf464fd3197606
SurajChinna <30693442+SurajChinna@users.noreply.github.com>
2017-08-22 07:52:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,8 @@
-console.log('Loaded!');
-
-//change the text of the main text div
-var element = document.getElementById("main-text");
-element.innerHTML = "New Value";
-
-//move the image
-var img = document.getElementById("madi");
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+//Counter Code
+var button = document.getElementById("counter");
+var count = document.getElementById("count");
+var counter = 0;
+button.onclick = function(){
+    counter = counter+1;
+    count.innerHTML = counter;
 };
