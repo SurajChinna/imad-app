5bdcb10204c6941b441ef2bae5f10ee563744455
SurajChinna <30693442+SurajChinna@users.noreply.github.com>
2017-09-02 14:41:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,12 +123,12 @@ app.get('/submit-name',function(req,res){
 
 app.get('/articles/:articleName', function (req, res) {
     
-    pool.query("SELECT * FROM article WHERE title = '"+req.params.articleName+"'", function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1",[articleName], function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else{
             if(result.rows.length === 0){
-                res.status(500).send('Artocle ont found');
+                res.status(500).send('Article ont found');
             } else{
                 var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
